7a8f2a560262baf7c585ff503f00e773fd61a472
LAPTOP-TQFM3LTV\dmsgh <dmsghs92@gmail.com>
2024-01-28 15:13:58 +0900

    [Feat] 테이블 추가

--- src/pages/Player/index.tsx
+++ src/pages/Player/index.tsx
@@ -1,3 +1,29 @@
+import { Stack, Box } from '@mui/material';
+import BasicTable from 'src/components/table/BasicTable';
+
+const headerSample = ['이름', '선출여부', '성별', '연령대', '프사', '대표팀'];
+
+const makeRow = (이름: string, 선출여부: string, 성별: string, 연령대: string, 프사: string, 대표팀: string) => {
+  return { 이름, 선출여부, 성별, 연령대, 프사, 대표팀 };
+};
+const rowsSample = [
+  { 이름: 'nameA', 선출여부: 'X', 성별: 'M', 연령대: '20', 프사: '없음', 대표팀: 'FC' },
+  makeRow('nameB', 'O', 'F', '30', '있음', 'FC'),
+  makeRow('nameB', 'O', 'F', '30', '있음', 'FC'),
+  makeRow('nameB', 'O', 'F', '30', '있음', 'FC'),
+];
+
 export default function Player() {
-  return <h1>Player</h1>;
+  return (
+    <Box alignContent='center' paddingX='15px'>
+      <Stack>
+        <h1>Player</h1>
+      </Stack>
+      <Stack> Filter </Stack>
+      <Stack>
+        <BasicTable header={headerSample} rows={rowsSample} />
+      </Stack>
+      <Stack> Pagination </Stack>
+    </Box>
+  );
 }
