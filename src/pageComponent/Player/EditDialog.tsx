b95513beebe71ffa43f3dc47ed454fec112dd174
LAPTOP-TQFM3LTV\dmsgh <dmsghs92@gmail.com>
2024-02-07 23:01:16 +0900

    Test: 테스트 세팅

--- src/pageComponent/Player/EditDialog.tsx
+++ src/pageComponent/Player/EditDialog.tsx
@@ -16,6 +16,7 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 
 import dayjs, { Dayjs } from 'dayjs';
+import 'dayjs/locale/ko';
 
 interface Props {
   player: Player;
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -8,16 +8,6 @@ declare interface YoutubeFormat {
 type Anchor = 'top' | 'left' | 'bottom' | 'right';
 type Gender = 'Male' | 'Female' | '혼성';
 
-declare interface PlayerData {
-  [key: string]: string;
-  이름: string;
-  선출여부: string;
-  성별: string;
-  연령대: string;
-  프사: string;
-  대표팀: string;
-}
-
 interface Player {
   id: number;
   name: string;
