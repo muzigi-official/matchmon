dea783d8467735ae46a8a4fc87181cb3d2cccb1b
LAPTOP-TQFM3LTV\dmsgh <dmsghs92@gmail.com>
2024-03-07 06:34:54 +0900

    chore: Storybook Viewport 추가

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,17 +2,49 @@ import React from 'react';
 
 import type { Preview } from '@storybook/react';
 import { ThemeProvider } from 'styled-components';
-
+import { BrowserRouter } from 'react-router-dom';
 import { merge } from 'ts-deepmerge';
 import { createTheme, responsiveFontSizes, Theme } from '@mui/material/styles';
+
 import { getDesignTokens, getThemedComponents } from '../src/theme/Theme';
 import GlobalStyle from '../src/styles/global-styles';
 
-import { BrowserRouter } from 'react-router-dom';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 const mode = 'light';
 let theme: Theme = createTheme(merge(getDesignTokens(mode), getThemedComponents(mode)));
 theme = responsiveFontSizes(theme);
+
+const customViewports = {
+  GalaxyNote20Ultra: {
+    name: 'Galaxy Note S20 Ultra',
+    styles: {
+      width: '412px',
+      height: '883px',
+    },
+  },
+  browser1024: {
+    name: 'Browser 1024px',
+    styles: {
+      width: '1024px',
+      height: '100%',
+    },
+  },
+  browser768: {
+    name: 'Browser 768px',
+    styles: {
+      width: '768px',
+      height: '100%',
+    },
+  },
+  browser500: {
+    name: 'Browser 500px',
+    styles: {
+      width: '500px',
+      height: '100%',
+    },
+  },
+};
 
 const preview: Preview = {
   parameters: {
@@ -22,6 +54,13 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    viewport: {
+      viewports: {
+        ...customViewports,
+        ...INITIAL_VIEWPORTS,
+      },
+      // defaultViewport: "GalaxyNote20Ultra",
     },
   },
 };
