0e9063d470d2bbfcb8196b61af1e2c3378de6cf4
JUHEEBAEK <bjh9287@naver.com>
2024-03-31 21:15:44 +0900

    Style: code 정리

--- src/pages/admin/competition/Details.tsx
+++ src/pages/admin/competition/Details.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getCompetition } from '@/api/competition';
-// import './index.scoped.css';
 
 export default function AdminCompetitionDetails() {
   const { competitionId } = useParams();
