87914dca893222e7694d71b898009e95231aed58
JUHEEBAEK <bjh9287@naver.com>
2024-03-31 15:25:28 +0900

    Feat: 기본 화면 세팅

---
+++ src/pages/admin/competition/Setting.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import '../index.scoped.css';
+
+export default function CompetitionSetting() {
+  return (
+    <div className='container'>
+      <h3>대회 설정</h3>
+    </div>
+  );
+}
