52756afea190ce76f5d8f716b552b570a07f53f3
JUHEEBAEK <bjh9287@naver.com>
2024-06-30 22:14:07 +0900

    Style: 기획대로 컬러 색 맞추기

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,7 +1,11 @@
+// colors.ts
 export const colors = {
-  primary: '#1E90FF',
-  secondary: '#FFD700',
-  background: '#F0F0F0',
-  text: '#333333',
+  primary: '#4A56E2',
+  secondaryDark: '#1F1B45',
+  secondaryLight: '#F4B400',
+  background: '#EFEFEF',
+  darkText: '#333333',
+  whiteText: '#FFFFFF',
   border: '#CCCCCC',
+  danger: '#D32F2F',
 };
