7c5fca39b9f72ebe9e93dc38393811ed5ff3d53d
JUHEEBAEK <bjh9287@naver.com>
2024-06-30 21:28:33 +0900

    Doc: design 시스템 초안 작성: 계속 여기에 추가하기

---
+++ src/styles/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+  primary: '#1E90FF',
+  secondary: '#FFD700',
+  background: '#F0F0F0',
+  text: '#333333',
+  border: '#CCCCCC',
+};
---
+++ src/styles/thema.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from 'styled-components';
+import { colors } from './colors';
+import { typography } from './typography';
+
+export const theme: DefaultTheme = {
+  colors,
+  typography,
+};
