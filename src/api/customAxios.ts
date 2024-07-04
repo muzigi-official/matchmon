9c6841d97d379ec1a7bd03cc84452f782a50c6cf
JUHEEBAEK <bjh9287@naver.com>
2024-07-05 00:12:08 +0900

    Fix: Cookie로 토큰 관리했던거 localStorage로 변경

--- src/api/customAxios.ts
+++ src/api/customAxios.ts
@@ -1,19 +1,18 @@
 import axios, { AxiosInstance } from 'axios';
-import cookies from 'js-cookie';
-
 import setupInterceptorsTo from './errorHandler';
 import { checkLogin } from './loginChecker';
 
 // const SERVER_ADDRESS = process.env.REACT_APP_BACK_END_POINT;
 const SERVER_ADDRESS = 'http://localhost:3000';
+const token = localStorage.getItem('access_token');
 
 const customAxios: AxiosInstance = axios.create({
   baseURL: `${SERVER_ADDRESS}`, // 기본 서버 주소 입력
   timeout: 6000,
   headers: {
     'Content-Type': 'application/json',
-    access_token: cookies.get('access_token'),
-    authorization: `Bearer ${cookies.get('access_token')}`,
+    access_token: token,
+    authorization: `Bearer ${token}`,
   },
 });
 
