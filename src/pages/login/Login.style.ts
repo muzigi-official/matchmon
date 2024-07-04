63b2114dc4aae50e461bce404a20565bff7e2bb8
JUHEEBAEK <bjh9287@naver.com>
2024-07-04 16:09:06 +0900

    Feat: Login page

---
+++ src/pages/login/Login.style.ts
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+import { colors } from '@/styles/colors';
+
+export const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 320px;
+  height: 100vh; /* 전체 높이를 차지하도록 설정 */
+  margin: auto;
+  text-align: center;
+`;
+
+export const Logo = styled.img`
+  width: 100px;
+  margin-bottom: 20px;
+`;
+
+export const LoginTitle = styled.h4`
+  margin: 20px 0;
+  font-size: 24px;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+export const LinksContainer = styled.div`
+  margin-top: 20px;
+  font-size: 0.75rem;
+`;
+
+export const StyledLink = styled.a`
+  color: ${colors.darkText};
+  text-decoration: none;
+  font-size: 0.75rem;
+  margin: 0 5px;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const OAuthContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 2%;
+  margin-top: 20px;
+  width: 100%;
+`;
