10119a16cce0b927b3f9262a85e6fda42d18a156
JUHEEBAEK <bjh9287@naver.com>
2024-07-05 00:07:17 +0900

    Fix: Store 구조 맞추기

--- src/store/useCompetitionStore.ts
+++ src/store/useCompetitionStore.ts
@@ -1,21 +1,24 @@
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
+import { devtools, persist, createJSONStorage } from 'zustand/middleware';
 
 interface CompetitionState {
   selectedCompetition: number | undefined;
   setCompetition: (competition: number) => void;
 }
 
-const useCompetitionStore = create(
-  persist<CompetitionState>(
-    set => ({
-      selectedCompetition: 0,
-      setCompetition: (competition: number) => set({ selectedCompetition: competition }),
-    }),
-    {
-      name: 'competitionStorage',
-    },
+const useUserStore = create<CompetitionState>()(
+  devtools(
+    persist(
+      set => ({
+        selectedCompetition: 0,
+        setCompetition: (competition: number) => set({ selectedCompetition: competition }),
+      }),
+      {
+        name: 'competition-storage',
+        storage: createJSONStorage(() => localStorage),
+      },
+    ),
   ),
 );
 
-export default useCompetitionStore;
+export default useUserStore;
