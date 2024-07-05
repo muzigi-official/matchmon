091874438305db71b445a0f41c92678745f57595
JUHEEBAEK <bjh9287@naver.com>
2024-07-05 20:09:21 +0900

    Feat: 대회 상태 util로 빼기

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -5,3 +5,17 @@ export function diffDate(start: string | Date, end: string | Date) {
   const endDate = dayjs(end);
   return endDate.diff(startDate, 'days');
 }
+
+export const getCompetitionStatus = (startDate: string, endDate: string): string => {
+  const today = new Date();
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+
+  if (today < start) {
+    return '대회예정';
+  } else if (today >= start && today <= end) {
+    return '진행중';
+  } else {
+    return '대회종료';
+  }
+};
