2e56c97ceb3be3416cc0851ff0093f46c510c1fc
JUHEEBAEK <bjh9287@naver.com>
2024-07-08 01:07:40 +0900

    Style: mixin 이름 변경 및 추가

--- src/styles/mixins.ts
+++ src/styles/mixins.ts
@@ -1,9 +1,23 @@
 import { css } from 'styled-components';
 
-export const containerMixin = css`
+export const containerFlexCol = css`
   display: flex;
   flex-direction: column;
   width: 100%;
   height: calc(var(--vh, 1vh) * 100 - var(--gnb-width));
   padding: 12px 0;
+`;
+
+export const containerFlexRow = css`
+  display: flex;
+  width: 100%;
+  height: calc(var(--vh, 1vh) * 100 - var(--gnb-width));
+  padding: 12px 0;
+  gap: 12px;
+`;
+
+export const flexPowerCenter = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
