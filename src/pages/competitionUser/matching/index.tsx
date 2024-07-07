a12b162ce2d5b8eb6d388675393035322997317d
JUHEEBAEK <bjh9287@naver.com>
2024-07-08 01:05:41 +0900

    Feat: 조짜기 페이지 왼쪽, 오른쪽으로 나누기

--- src/pages/competitionUser/matching/index.tsx
+++ src/pages/competitionUser/matching/index.tsx
@@ -1,18 +1,26 @@
 import { useState, useEffect } from 'react';
 
-import useCompetitionStore from '@/store/useCompetitionStore';
 import { getParticipateTeams } from '@/api/joinTeamComp';
+import useCompetitionStore from '@/store/useCompetitionStore';
 
 import * as S from './Index.style';
+import TeamList from './ParticipateTeamList';
+import GroupList from './GroupList';
+
+interface IGroup {
+  id: number;
+  name: string;
+  teams: ITeam[];
+}
 
 export default function MatchingPage() {
   const { selectedCompetition } = useCompetitionStore();
   const [teams, setTeams] = useState<IJoinCompTeam[]>([]);
+  const [groups, setGroups] = useState<IGroup[]>([]);
 
   const getJoinTeams = async () => {
     if (selectedCompetition) {
       const response = await getParticipateTeams(selectedCompetition);
-      console.log(response);
       const parseTeams = response.map(item => {
         const { id, team, participateState } = item;
         return {
@@ -28,30 +36,53 @@ export default function MatchingPage() {
     }
   };
 
+  const handleAddGroup = () => {
+    console.log('add group');
+    console.log('open dialog');
+  };
+
   useEffect(() => {
     // 팀 리스트 부르기
     getJoinTeams();
     // 조 리스트 부르기
   }, []);
 
+  useEffect(() => {
+    // TODO: 그룹 리스트를 불러오는 로직 추가
+    // 임시로 더미 데이터를 사용
+    const dummyGroups = [
+      {
+        id: 1,
+        name: 'Group A',
+        teams: [
+          { id: 1, name: 'Team 1', emblem: '/empty_emblem.png', location: 'Seoul', gender: 'F' },
+          { id: 2, name: 'Team 2', emblem: '/empty_emblem.png', location: 'Seoul', gender: 'F' },
+        ],
+      },
+      {
+        id: 2,
+        name: 'Group B',
+        teams: [
+          { id: 3, name: 'Team 3', emblem: '/empty_emblem.png', location: 'Seoul', gender: 'F' },
+          { id: 4, name: 'Team 4', emblem: '/empty_emblem.png', location: 'Seoul', gender: 'F' },
+        ],
+      },
+    ];
+    setGroups(dummyGroups);
+  }, []);
+
   return (
     <S.Container>
-      <S.Content>
+      <S.LeftPanel>
         <S.Header>
           <h5>참가 명단</h5>
           <h5>{teams.length} 명</h5>
         </S.Header>
-        <S.List>
-          {teams.map(team => {
-            return (
-              <S.ListItem key={team.joinCompId}>
-                {team.emblem ? <span>{team.emblem}</span> : <span>{team.name.charAt(0)}</span>}
-                <span>{team.name}</span>
-              </S.ListItem>
-            );
-          })}
-        </S.List>
-      </S.Content>
+        <TeamList teams={teams} />
+      </S.LeftPanel>
+      <S.RightPanel>
+        <GroupList groups={groups} onAddGroup={handleAddGroup} />
+      </S.RightPanel>
     </S.Container>
   );
 }
