c287a8fb8ae4474183007864094ebf3cac93f3fc
JUHEEBAEK <bjh9287@naver.com>
2024-07-21 22:12:05 +0900

    Feat: 경기 시간표 입력 페이지 세팅

--- src/pages/competitionUser/bracket/index.tsx
+++ src/pages/competitionUser/bracket/index.tsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import { useState } from 'react';
 
-import '../index.scoped.css';
+import Button from '@/components/common/Button';
+import MatchGenerator from './DialogMatchGenerator';
+import * as S from './Index.style';
+
+export interface IEvent {
+  name: string;
+  time: string;
+}
+
+export interface IFormData {
+  startTime: string;
+  matchDuration: number;
+  restTime: number;
+  halfTimeRest: boolean;
+  stadiums: string[];
+  events: IEvent[];
+}
 
 export default function AdmiBracket() {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const toggleDialog = () => {
+    setIsDialogOpen(!isDialogOpen);
+  };
+
+  const configureMatchSettings = (formData: IFormData) => {
+    console.log('Generated Schedule:', formData);
+    setIsDialogOpen(false);
+  };
+
   return (
-    <div className='container'>
-      <h3>대진표</h3>
-    </div>
+    <S.Container className='container'>
+      <S.Top>
+        <S.Title>
+          <span>시간표/대진표</span>
+        </S.Title>
+      </S.Top>
+      <S.Actions>
+        <p> 경기설정 세부 정보 </p>
+        <Button color='primary' onClick={toggleDialog}>
+          경기 설정
+        </Button>
+      </S.Actions>
+      <MatchGenerator open={isDialogOpen} onClose={toggleDialog} onSubmit={configureMatchSettings} />
+    </S.Container>
   );
 }
