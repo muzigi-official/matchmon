97fd7069f9c0a82fb13936e355539499a14f03ad
JUHEEBAEK <bjh9287@naver.com>
2024-07-23 02:18:45 +0900

    Feat: Match Field 탭 으로 구분

---
+++ src/pages/competitionUser/bracket/StadiumTabs.tsx
@@ -0,0 +1,43 @@
+import Tabs from '@/components/common/Tabs';
+import MatchField from './MatchField';
+import * as S from './Index.style';
+import Button from '@/components/common/Button';
+
+interface IStadiumTabsProps {
+  schedules: IMatchSchedule[];
+  addMatch: () => void;
+  removeMatch: (index: number) => void;
+  handleMatchChange: (index: number, field: keyof IMatchSchedule, value: string) => void;
+}
+
+const StadiumTabs = ({ schedules, addMatch, removeMatch, handleMatchChange }: IStadiumTabsProps) => {
+  const stadiums = Array.from(new Set(schedules.map(match => match.stadium)));
+
+  const renderMatchFields = (filteredMatches: IMatchSchedule[]) => (
+    <S.TimeTable>
+      {filteredMatches.map((match, index) => (
+        <MatchField key={index} match={match} index={index} onMatchChange={handleMatchChange} onRemove={removeMatch} />
+      ))}
+      <Button onClick={addMatch}>추가</Button>
+    </S.TimeTable>
+  );
+
+  const tabs = [
+    {
+      label: '전체',
+      content: renderMatchFields(schedules),
+    },
+    ...stadiums.map(stadium => ({
+      label: stadium,
+      content: renderMatchFields(schedules.filter(match => match.stadium === stadium)),
+    })),
+  ];
+
+  return (
+    <div>
+      <Tabs tabs={tabs} />
+    </div>
+  );
+};
+
+export default StadiumTabs;
