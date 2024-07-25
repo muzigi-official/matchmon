cbb19d07155ee505c95f53f8727f49c4d879fb0a
JUHEEBAEK <bjh9287@naver.com>
2024-07-26 00:43:58 +0900

    Fix: IMatchSettingParams type 맞추기

--- src/api/matchSetting.ts
+++ src/api/matchSetting.ts
@@ -1,12 +1,12 @@
 import requestAPI from '@/utils/api';
 
-export const fetchMatchSettings = (competitionId: number): Promise<IUpdateMatchSettingParams[]> =>
+export const fetchMatchSettings = (competitionId: number): Promise<IMatchSettingParams[]> =>
   requestAPI.get(`/match-settings?competitionId=${competitionId}`);
 
-export const createMatchSetting = (params: ICreateMatchSettingParams): Promise<IUpdateMatchSettingParams> =>
+export const createMatchSetting = (params: ICreateMatchSettingParams): Promise<IMatchSettingParams> =>
   requestAPI.post(`/match-settings`, params);
 
-export const updateMatchSetting = (params: IUpdateMatchSettingParams): Promise<IUpdateMatchSettingParams> =>
+export const updateMatchSetting = (params: IMatchSettingParams): Promise<IMatchSettingParams> =>
   requestAPI.put(`/match-settings/${params.id}`, params);
 
 export const deleteMatchSetting = (id: number): Promise<void> => requestAPI.delete(`/match-settings/${id}`);
